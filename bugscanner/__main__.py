4b4ae2ea2e1049264854595b6c73aab7937a1344
clnMox <122692346+clnMox@users.noreply.github.com>
2024-07-27 11:29:41 +0300

    update

--- bugscanner/__main__.py
+++ bugscanner/__main__.py
@@ -3,93 +3,94 @@ import sys
 
 from .direct_scanner import DirectScanner
 
-
 def get_arguments():
-	parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=52))
-	parser.add_argument(
-		'filename',
-		help='filename',
-		type=str,
-	)
-	parser.add_argument(
-		'--mode',
-		help='mode',
-		dest='mode',
-		choices=('direct', 'proxy', 'ssl', 'udp'),
-		type=str,
-		default='direct',
-	)
-	parser.add_argument(
-		'--method',
-		help='method list',
-		dest='method_list',
-		type=str,
-		default='head',
-	)
-	parser.add_argument(
-		'--port',
-		help='port list',
-		dest='port_list',
-		type=str,
-		default='80',
-	)
-	parser.add_argument(
-		'--proxy',
-		help='proxy',
-		dest='proxy',
-		type=str,
-		default='',
-	)
-	# parser.add_argument(
-	# 	'--deep',
-	# 	help='subdomain deep',
-	# 	dest='deep',
-	# 	type=int,
-	# )
-	parser.add_argument(
-		'--output',
-		help='output file name',
-		dest='output',
-		type=str,
-	)
-	parser.add_argument(
-		'--threads',
-		help='threads',
-		dest='threads',
-		type=int,
-	)
+    parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=52))
+    parser.add_argument(
+        'filename',
+        help='filename',
+        type=str,
+    )
+    parser.add_argument(
+        '--mode',
+        help='mode',
+        dest='mode',
+        choices=('direct', 'proxy', 'ssl', 'udp'),
+        type=str,
+        default='direct',
+    )
+    parser.add_argument(
+        '--method',
+        help='method list',
+        dest='method_list',
+        type=str,
+        default='head',
+    )
+    parser.add_argument(
+        '--port',
+        help='port list',
+        dest='port_list',
+        type=str,
+        default='80',
+    )
+    parser.add_argument(
+        '--proxy',
+        help='proxy',
+        dest='proxy',
+        type=str,
+        default='',
+    )
+    # parser.add_argument(
+    #     '--deep',
+    #     help='subdomain deep',
+    #     dest='deep',
+    #     type=int,
+    # )
+    parser.add_argument(
+        '--output',
+        help='output file name',
+        dest='output',
+        type=str,
+    )
+    parser.add_argument(
+        '--threads',
+        help='threads',
+        dest='threads',
+        type=int,
+    )
 
-	return parser.parse_args()
-
+    return parser.parse_args()
 
 def main():
-	arguments = get_arguments()
+    arguments = get_arguments()
 
-	method_list = arguments.method_list.split(',')
-	host_list = open(arguments.filename).read().splitlines()
-	port_list = arguments.port_list.split(',')
-	proxy = arguments.proxy.split(':')
+    method_list = arguments.method_list.split(',')
+    host_list = open(arguments.filename).read().splitlines()
+    port_list = arguments.port_list.split(',')
+    proxy = arguments.proxy.split(':')
 
-	if arguments.mode == 'direct':
-		scanner = DirectScanner()
+    if arguments.mode == 'direct':
+        scanner = DirectScanner()
 
-	elif arguments.mode == 'proxy':
-		if not proxy or len(proxy) != 2:
-			sys.exit('--proxy host:port')
+    elif arguments.mode == 'proxy':
+        if not proxy or len(proxy) != 2:
+            sys.exit('--proxy host:port')
 
-	else:
-		sys.exit('Not Available!')
+    else:
+        sys.exit('Not Available!')
 
-	scanner.method_list = method_list
-	scanner.host_list = host_list
-	scanner.port_list = port_list
-	scanner.threads = arguments.threads
-	scanner.start()
+    scanner.method_list = method_list
+    scanner.host_list = host_list
+    scanner.port_list = port_list
+    scanner.threads = arguments.threads
+    scanner.start()
 
-	if arguments.output:
-		with open(arguments.output, 'w+') as file:
-			file.write('\n'.join([ str(x) for x in scanner.success_list() ]) + '\n')
+    if arguments.output:
+        with open(arguments.output, 'w+') as file:
+            file.write('\n'.join([str(x) for x in scanner.success_list()]) + '\n')
 
+    # Print the stored domains with status codes
+    for domain in scanner.domains_with_status:
+        print(domain)
 
 if __name__ == '__main__':
-	main()
+    main()
--- bugscanner/direct_scanner.py
+++ bugscanner/direct_scanner.py
@@ -1,100 +1,104 @@
 from .bug_scanner import BugScanner
 
-
 class DirectScanner(BugScanner):
-	method_list = []
-	host_list = []
-	port_list = []
+    method_list = []
+    host_list = []
+    port_list = []
+    domains_with_status = []  # List to store domains with status codes
 
-	def log_info(self, **kwargs):
-		for x in ['color', 'status_code', 'server']:
-			kwargs[x] = kwargs.get(x, '')
+    def log_info(self, **kwargs):
+        for x in ['color', 'status_code', 'server']:
+            kwargs[x] = kwargs.get(x, '')
 
-		W2 = self.logger.special_chars['W2']
-		G1 = self.logger.special_chars['G1']
-		P1 = self.logger.special_chars['P1']
-		CC = self.logger.special_chars['CC']
+        W2 = self.logger.special_chars['W2']
+        G1 = self.logger.special_chars['G1']
+        P1 = self.logger.special_chars['P1']
+        CC = self.logger.special_chars['CC']
 
-		if not kwargs['status_code']:
-			kwargs['color'] = W2
+        if not kwargs['status_code']:
+            kwargs['color'] = W2
 
-		kwargs['CC'] = CC
+        kwargs['CC'] = CC
 
-		location = kwargs.get('location')
+        location = kwargs.get('location')
 
-		if location:
-			if location.startswith(f"https://{kwargs['host']}"):
-				kwargs['status_code'] = f"{P1}{kwargs['status_code']:<4}"
-			else:
-				kwargs['host'] += f"{CC} -> {G1}{location}{CC}"
+        if location:
+            if location.startswith(f"https://{kwargs['host']}"):
+                kwargs['status_code'] = f"{P1}{kwargs['status_code']:<4}"
+            else:
+                kwargs['host'] += f"{CC} -> {G1}{location}{CC}"
 
-		messages = []
+        messages = []
 
-		for x in ['{method:<6}', '{status_code:<4}', '{server:<22}', '{port:<4}', '{host}']:
-			messages.append(f'{{color}}{x}{{CC}}')
+        for x in ['{method:<6}', '{status_code:<4}', '{server:<22}', '{port:<4}', '{host}']:
+            messages.append(f'{{color}}{x}{{CC}}')
 
-		super().log('  '.join(messages).format(**kwargs))
+        super().log('  '.join(messages).format(**kwargs))
 
-	def get_task_list(self):
-		for method in self.filter_list(self.method_list):
-			for host in self.filter_list(self.host_list):
-				for port in self.filter_list(self.port_list):
-					yield {
-						'method': method.upper(),
-						'host': host,
-						'port': port,
-					}
+        # Store the domain with status code
+        if kwargs['status_code']:
+            self.domains_with_status.append(kwargs['host'])
 
-	def init(self):
-		super().init()
+    def get_task_list(self):
+        for method in self.filter_list(self.method_list):
+            for host in self.filter_list(self.host_list):
+                for port in self.filter_list(self.port_list):
+                    yield {
+                        'method': method.upper(),
+                        'host': host,
+                        'port': port,
+                    }
 
-		self.log_info(method='Method', status_code='Code', server='Server', port='Port', host='Host')
-		self.log_info(method='------', status_code='----', server='------', port='----', host='----')
+    def init(self):
+        super().init()
 
-	def task(self, payload):
-		method = payload['method']
-		host = payload['host']
-		port = payload['port']
+        self.log_info(method='Method', status_code='Code', server='Server', port='Port', host='Host')
+        self.log_info(method='------', status_code='----', server='------', port='----', host='----')
 
-		response = self.request(method, self.get_url(host, port), retry=1, timeout=3, allow_redirects=False)
+    def task(self, payload):
+        method = payload['method']
+        host = payload['host']
+        port = payload['port']
 
-		G1 = self.logger.special_chars['G1']
-		G2 = self.logger.special_chars['G2']
+        response = self.request(method, self.get_url(host, port), retry=1, timeout=3, allow_redirects=False)
 
-		data = {
-			'method': method,
-			'host': host,
-			'port': port,
-		}
+        G1 = self.logger.special_chars['G1']
+        G2 = self.logger.special_chars['G2']
 
-		if response is not None:
-			color = ''
-			status_code = response.status_code
-			server = response.headers.get('server', '')
-			location = response.headers.get('location', '')
+        data = {
+            'method': method,
+            'host': host,
+            'port': port,
+        }
 
-			if server in ['AkamaiGHost']:
-				if status_code == 400:
-					color = G1
-				else:
-					color = G2
+        if response is not None:
+            color = ''
+            status_code = response.status_code
+            server = response.headers.get('server', '')
+            location = response.headers.get('location', '')
 
-			elif server in ['Varnish']:
-				if status_code == 500:
-					color = G1
+            if server in ['AkamaiGHost']:
+                if status_code == 400:
+                    color = G1
+                else:
+                    color = G2
 
-			elif server in ['AkamaiNetStorage']:
-				color = G2
+            elif server in ['Varnish']:
+                if status_code == 500:
+                    color = G1
 
-			data_success = {
-				'color': color,
-				'status_code': status_code,
-				'server': server,
-				'location': location,
-			}
+            elif server in ['AkamaiNetStorage']:
+                color = G2
 
-			data = self.dict_merge(data, data_success)
+            data_success = {
+                'color': color,
+                'status_code': status_code,
+                'server': server,
+                'location': location,
+            }
 
-			self.task_success(data)
+            data = self.dict_merge(data, data_success)
 
-		self.log_info(**data)
+            self.task_success(data)
+
+        self.log_info(**data)
